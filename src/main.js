0d4baa3ee6daf09a1de63d23234173a27eb88f21
Samu Gergely <samugergely99@gmail.com>
2024-11-25 09:41:35 +0100

    init store

--- src/main.js
+++ src/main.js
@@ -1,20 +1,16 @@
-/**
- * main.js
- *
- * Bootstraps Vuetify and other plugins then mounts the App`
- */
-
 // Plugins
 import { registerPlugins } from '@/plugins'
-
 // Components
 import App from './App.vue'
-
 // Composables
 import { createApp } from 'vue'
+import { useAppStore } from './stores/app'
 
 const app = createApp(App)
 
 registerPlugins(app)
+
+const exampleStore = useAppStore();
+exampleStore.init();
 
 app.mount('#app')
--- src/stores/app.js
+++ src/stores/app.js
@@ -1,8 +1,25 @@
 // Utilities
+import { get } from '@/services/movieService'
 import { defineStore } from 'pinia'
 
 export const useAppStore = defineStore('app', {
   state: () => ({
-    //
+    catalogList: [],
+    page: 0,
+    type: 'movie',
+    order: 'popular',
   }),
+
+  actions: {
+    async fetchCatalogList () {
+      this.catalogList = await get(this.page, this.type, this.order);
+      this.page++;
+    },
+
+    async init() {
+      if (!this.catalogList.length) {
+        await this.fetchCatalogList();
+      }
+    }
+  },
 })
